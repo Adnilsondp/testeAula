e2963b7551dc98ea7186631999c5e3d68813424d
ADNILSON DE PAULA DOS SANTOS <133922383+Adnilsondp@users.noreply.github.com>
2025-09-13 17:39:03 -0300

    teste

---
+++ pythonProject1/main.py
@@ -0,0 +1,9 @@
+# Solicita os dois números ao usuário
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+# Calcula a soma
+soma = num1 + num2
+
+# Exibe o resultado
+print("A soma dos números é:", soma)
